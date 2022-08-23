d0871df2908c526e130387dec6000f1f7061e7f3
natiman58 <natiman58@gmail.com>
2022-08-23 21:51:24 +0300

    &&

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,7 +6,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if ((node->right == NULL && node->left == NULL) || node == NULL)
+	if ((node->right == NULL && node->left == NULL) && node != NULL)
 		return (1);
 	return (0);
 }
