831492dfd7bb47c2a95eb6c87f942255bb6d1200
natiman58 <natiman58@gmail.com>
2022-08-23 16:35:10 +0300

    new

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,20 +11,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	if (!parent)
 		return (NULL);
-	binary_tree_t *new_Node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_Node = binary_tree_node(parent, value);
 
 	if (!new_Node)
 		return (NULL);
 
-	new_Node->n = value;
-	new_Node->parent = parent;
-	new_Node->right = parent->right;
-	new_Node->left = NULL;
-
 	if (parent->right != NULL)
 	{
+		new_Node->right = parent->right;
 		parent->right->parent = new_Node;
 	}
 	parent->right = new_Node;
+
 	return (new_Node);
 }
